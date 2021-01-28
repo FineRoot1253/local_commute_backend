77bcea8d97e7c9073fe129d68d01a7ad0e6029ce
JunGeunHong1129 <gjhong1129@gmail.com>
2021-01-28 09:19:00 +0900

    21_01_28 홍준근 first commit

---
+++ local_commute/dao/model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: {type: String, required: true, unique: true},
+    userNm: {type: String, required: true},
+    isCommuted: {type: Boolean, required: true},
+},{
+    timestamps: true
+});
+
+userSchema.statics.create = function (payload){
+    console.log(payload);
+    const user = new this(payload);
+    return user.save();
+};
+
+userSchema.statics.findAll = () => this.find({});
+
+userSchema.statics.findOneByUserId = function (userId) {
+    console.log(userId);
+    return this.findOne({userId});
+}
+
+userSchema.statics.updateByUserId = function(userId, payload) {
+    return this.findOneAndUpdate({userId}, payload, {new: true});
+} 
+
+userSchema.statics.deleteByUserId = function(userId) {
+    return this.remove({userId});
+} 
+
+module.exports = mongoose.model('user', userSchema);
---
+++ local_commute/routes/index.js
@@ -0,0 +1,57 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../dao/model');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  User.findAll()
+  .then((users)=>{
+    if(!users) return res.status(404).send({err: 'User Not Found'});
+    res.send(users);
+  })
+  .catch((err) => {
+    console.log(err);
+    return res.status(500).send(err);
+  });
+});
+
+router.get('/user/:userId', function(req, res, next) {
+  console.log(req.params);
+  User.findOneByUserId(req.params.userId)
+  .then((user)=>{
+    console.log(user);
+    if(!user) return res.status(404).send({err: 'User Not Found'});
+    res.send(user);
+  })
+  .catch((err) => {
+    console.log(err);
+    return res.status(500).send(err);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  User.create(req.body)
+  .then((user)=>res.sendStatus(200))
+  .catch((err) => {
+    console.log(err);
+    return res.status(500).send(err);
+  });
+});
+
+router.put('/user/:userId', function(req, res, next) {
+  User.updateByUserId(req.params.userId, req.body)
+  .then((user)=>{
+    res.send(user);
+  })
+  .catch(err => res.status(500).send(err));
+});
+
+router.delete('/user/:userId', function(req, res, next) {
+  User.deleteByUserId(req.params.userId)
+  .then((user)=>res.sendStatus(200))
+  .catch(err => res.status(500).send(err));
+});
+
+module.exports = router;
